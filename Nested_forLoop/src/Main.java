1062486435aed34f60a1642bf31924fc24c98456
kellel14 <kellel14@gmail.com>
2019-06-17 13:28:06 -0400

    intial push: nested for loop

---
+++ Nested_forLoop/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+        public static void main(String[] args) {
+            // write your code here
+            for (int line = 1; line <= 9; line++) {
+                for (int index = 1; index <= line; index++) {
+                    System.out.print(line);
+                }
+                System.out.println();
+            }
+        }
+}
